42c9dae456e81012a5e965dc3f6e2bf0ea4e9895
XenoBelino <xenobelino@gmail.com>
2025-08-14 18:13:42 +0200

    Delete ffmpeg-instance.js

--- ffmpeg-instance.js
+++
@@ -1,4 +0,0 @@
-// ffmpeg-instance.js
-import { createFFmpeg, fetchFile } from 'https://cdn.jsdelivr.net/npm/@ffmpeg/ffmpeg@0.11.6/+esm';
-const ffmpeg = createFFmpeg({ log: true });
-export { ffmpeg, fetchFile };
